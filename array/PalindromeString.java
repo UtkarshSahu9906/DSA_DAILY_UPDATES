888ee7235eef18ee2e7275afc9cc5e19a20732b3
UtkarshSahu9906 <utkarshsahu9906@gmail.com>
2024-10-14 00:31:34 +0530

    improve the time complexity

--- array/PalindromeString.java
+++ array/PalindromeString.java
@@ -4,35 +4,33 @@ import javax.print.DocFlavor;
 public class PalindromeString {
 
     public static void main(String[] args) {
-        String s = Palindrome("abcddcba");
-        System.out.println(s);
-        System.out.println(checkStr(s));
+
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
 
     }
 
-    static String Palindrome(String s) {
-        String str = "";
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z') {
-                str += s.charAt(i);
-            } else if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z') {
-                char changedChar = (char) (s.charAt(i) + 32);
-                str += changedChar;
+    
+        static  boolean isPalindrome(String s) {
+            StringBuilder str = new StringBuilder();
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z') {
+                    str.append(s.charAt(i));
+                } else if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+                    str.append(s.charAt(i));
+                } else if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z') {
+                    char changedChar = (char) (s.charAt(i) + 32);
+                    str.append(changedChar);
+                }
             }
-        }
-
-        return str;
-    }
-
-    static boolean checkStr(String str) {
-        for (int j = 0; j < str.length(); j++) {
-            if (str.charAt(j) == str.charAt(str.length() - j - 1)) {
-
-            } else if (str.charAt(j) != str.charAt(str.length() - j - 1)) {
-                return false;
+    
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) == str.charAt(str.length() - j - 1)) {
+    
+                } else if (str.charAt(j) != str.charAt(str.length() - j - 1)) {
+                    return false;
+                }
+    
             }
-
+            return true;
         }
-        return true;
     }
-}
